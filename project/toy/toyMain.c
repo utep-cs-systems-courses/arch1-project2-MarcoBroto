9dc4d10d49efd03bbcb47dbfd9f893c1be6289d4
MarcoBroto <msoto9940@gmail.com>
2020-03-29 05:02:31 -0600

    Needs bug fixes with registering switches

--- project/toy/toyMain.c
+++ project/toy/toyMain.c
@@ -6,13 +6,14 @@
 #include "buzzer.h"
 
 
+static FILE *log;
 static enum {S1=0, S2=1, S3=2, S4=3, S5=5} state = S1;
 static char sound_ind = 0;
 static int sounds[][10] = {
-  {10000, 6000, 0},
-  {8000, 6000, 0},
-  {4000, 8000, 0},
-  {8000, 8000, 8000, 4500, 8000, 8000, 4500, 6000, 0}
+	{10000, 6000, 0},
+	{8000, 6000, 0},
+	{4000, 8000, 0},
+	{8000, 8000, 8000, 4500, 8000, 8000, 4500, 6000, 1}
 };
 static int soundLen[] = {3, 3, 3, 9}; 
 
@@ -21,22 +22,29 @@ void static stateAdvance() {
 	switch (state) {
 		case S1: // Play low two-tone sound byte, red on, green off
 		  red_on = 0; green_on = 0;
+			led_changed = 1;
+			state = S2;
 			break;
 		case S2: // Play higher two-tone sound byte, red off, green on
 			red_on = 1; green_on = 0;
+			led_changed = 1;
+			state = S3;
 			break;
 		case S3: // Play highest two-tone sound byte, red on, green on
 			red_on = 0; green_on = 1;
+			led_changed = 1;
+			state = S4;
 			break;
 		case S4:
 			red_on = 1; green_on = 1;
+			led_changed = 1;
+			state = S5;
 			break;
-	case S5: // Play win sound, reset to first state
+		default: // Play win sound, reset to first state
 		  red_on = 0; green_on = 1;
+		  led_changed = 1;
+			state = S1;
 			break;
-	default:
-	  red_on = 0; green_on = 0;
-	  state = S1;
 	}
 	led_changed = 1;
 	led_update();
@@ -44,7 +52,7 @@ void static stateAdvance() {
 
 
 static void soundStateAdvance() {
-  if (sound_ind < soundLen[state])
+  while (sound_ind++ < soundLen[state])
     buzzer_set_period(sounds[state][sound_ind++]);
 }
 
@@ -53,38 +61,33 @@ void __interrupt_vec(PORT1_VECTOR) Port_1() {   // Switch on P1 (S2)
   if (P1IFG & SWITCHES) {	      // did a button cause this interrupt?
     P1IFG &= ~SWITCHES;		      // clear pending sw interrupts
     switch_interrupt_handler();	// single handler for all switches
+    fprintf(log, "Switch Interrupt:\nswitch_state_down=%d\nswitch_state_changed=%d\n\n");
 		if (switch_state_down && switch_state_changed) {
-		  state += 1;
-		  sound_ind = 0;
-		  switch_state_changed = 0;
-		  stateAdvance();
+			stateAdvance();
+			switch_state_changed = 0;
 		}
-		if (state > S5 || state < S1) state = S1;
 	}
 }
 
 
 void __interrupt_vec(WDT_VECTOR) WDT() { // 250 interrupts/sec
-  static char timerCount = 0;
-	if (timerCount % 125 == 0) { // Every ~1/4 second
+  static char blink_count = 0;
+	if (++blink_count == 60) { // Every 0.5 second
 		soundStateAdvance();
-	}
-	if (state == 5 && timerCount % 60 == 0) { // Every 1/4 second
+		if (blink_count == 30 && state == S5) {
 		  green_on ^= 1; red_on ^= 1;
-		  led_changed = 1;
-		  led_update();
+		}
 	}
-	timerCount = (timerCount >= 250) ? 0 : timerCount++;
 }
 
 
 int main() {
-  configureClocks();
+  log = fopen("output.log", "a+");
 	buzzer_init();
 	buzzer_set_period(0);
 	led_init();
 	switch_init();
-	//enableWDTInterrupts();
+	enableWDTInterrupts();
 	or_sr(0x18);
 	return 0;
 }
