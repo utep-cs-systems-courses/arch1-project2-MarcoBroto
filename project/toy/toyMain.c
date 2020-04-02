e5ebedb8eaa69e03f7462c8ae5c40c1dea7c4ded
MarcoBroto <msoto9940@gmail.com>
2020-04-02 03:29:17 -0600

    IDK

--- project/toy/toyMain.c
+++ project/toy/toyMain.c
@@ -7,7 +7,7 @@
 
 static enum {S1=0, S2=1, S3=2, S4=3, S5=4} state = S1;
 static char sound_ind = 0;
-static int sounds[][17] = {
+static int sounds[][18] = {
 	{0},
 	{0, 10000, 6000, 0},
 	{0, 8000, 4000, 0},
@@ -23,18 +23,15 @@ void static stateAdvance() {
 			red_on = 0; green_on = 0;
 			break;
 		case S2: // Play higher two-tone sound byte, red off, green on
-			red_on = 1; green_on = 0;
+			red_on = 1; green_on = 1;
 			break;
 		case S3: // Play highest two-tone sound byte, red on, green on
 			red_on = 0; green_on = 1;
 			break;
 		case S4:
-			red_on = 1; green_on = 1;
+			red_on = 1; green_on = 0;
 			break;
 		case S5: // Play win sound, reset to first state
-			if (red_on == green_on) {
-				red_on = 0; green_on = 1;
-			}
 			red_on ^= 1; green_on ^= 1; // Toggle both LEDs
 			break;
 		default:
@@ -56,8 +53,9 @@ static void soundStateAdvance() {
 void __interrupt_vec(PORT1_VECTOR) Port_1() {   // Switch on P1 (S2)
 	static char p1val, prev_p1val;
 	if (P1IFG & SWITCHES) {	      // did a button cause this interrupt?
-		P1IFG &= ~SWITCHES;		      // clear pending sw interrupts
-		p1val = ~switch_interrupt_handler(); // Store switch press value
+		P1IFG &= ~SWITCHES; // clear pending sw interrupts
+		switch_interrupt_handler();
+		p1val = ~switch_state_down; // Store switch press value
 		if (p1val & (p1val ^ prev_p1val)) { // Only register press if switch was in off state
 			state = (state > S5 || state < S1) ? S1 : state+1;
 			sound_ind = 0; // Reset sound frequency state index
