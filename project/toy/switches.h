8b6f1423606d08204cc6ac47c048ffc10bbf6921
MarcoBroto <msoto9940@gmail.com>
2020-04-03 21:40:29 -0600

    Changed switch handler return value

--- project/toy/switches.h
+++ project/toy/switches.h
@@ -5,7 +5,7 @@
 #define SWITCHES SW1		/* only 1 switch on this board */
 
 void switch_init();
-void switch_interrupt_handler();
+char switch_interrupt_handler();
 
 extern char switch_state_down, switch_state_changed; /* effectively boolean */
 
